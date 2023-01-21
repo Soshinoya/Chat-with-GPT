0f24194bced56e3f93a57b9d7c3a932539c67f18
Alex <nike95125@gmail.com>
2023-01-21 14:48:45 +0300

    start project

---
+++ src/hooks/useCustomModal/useCustomModal.jsx
@@ -0,0 +1,19 @@
+import Modal from './Modal'
+import './bootstrap.min.css'
+import './modal.css'
+
+const useCustomModal = options => {
+
+    const {modalInDOM, modalJSX} = Modal({ ...options })
+
+    const modal = {
+        modalJSX,
+        modalOpen() {
+            modalInDOM.current.classList.add('open')
+        }
+    }
+
+    return modal
+}
+
+export default useCustomModal
